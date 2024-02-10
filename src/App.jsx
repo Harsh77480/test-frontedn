963fc2776ac7884434e501d10d7986df85a94c3e
Harsh77480 <otherworks77480@gmail.com>
2024-02-10 22:43:35 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
-import { Route } from 'react-router-dom'
-import { Chat } from './pages/chat'
-import CanvasComponent from './components/DrawingCanvas'
-import DrawingCanvas from './components/DrawingCanvas'
-import { Login } from './components/Login'
-import { SignUp } from './components/SignUp'
+// import { Route } from 'react-router-dom'
+// import CanvasComponent from './components/DrawingCanvas'
+// import DrawingCanvas from './components/DrawingCanvas'
+// import { Login } from './components/Login'
+// import { SignUp } from './components/SignUp'
 import { Home } from './pages/Home/Home'
 function App() {
 
